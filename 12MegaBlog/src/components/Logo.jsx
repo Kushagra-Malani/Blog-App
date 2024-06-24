ba060c2a24426c6e08e0589c5658174e4923a0b7
Kushagra <155226608+Kushagra-Malani@users.noreply.github.com>
2024-06-24 13:20:59 +0530

    updated logo and toplogo url

--- 12MegaBlog/src/components/Logo.jsx
+++ 12MegaBlog/src/components/Logo.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 function Logo() {
     return (
         <div>
-            <img src="\src\assets\blogAppLogo.png" alt="Logo" />
+            {/* <img src="\src\assets\blogAppLogo.png" alt="Logo" /> */}
+            <img src="https://res.cloudinary.com/dqlgd3hef/image/upload/blogAppLogo_xe1sph.png" alt="Logo" />
         </div>
     );
 }
--- 12MegaBlog/src/components/TopLogo.jsx
+++ 12MegaBlog/src/components/TopLogo.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 function TopLogo() {
     return (
         <div>
-            <img className='w-[400px] h-[180px]' src="\src\assets\topLogo.png" alt="Logo" />
+            {/* <img className='w-[400px] h-[180px]' src="\src\assets\topLogo.png" alt="Logo" /> */}
+            <img className='w-[400px] h-[180px]' src="https://res.cloudinary.com/dqlgd3hef/image/upload/topLogo_l6onu0.png" alt="Logo" />
         </div>
-    );
+    );    
 }
 
 export default TopLogo;
